58a951e038d7d971f67840a27f1bc815d6319e67
auryouth <ausosa528@proton.me>
2024-01-28 16:09:58 +0800

    update

--- delete-asset-action/entrypoint.sh
+++ delete-asset-action/entrypoint.sh
@@ -10,12 +10,12 @@ chmod -R a+rw .
 
 pacman-key --init
 
-assets=($(gh release view -R $repo_full --json assets | jq -cr '.assets[].name'  |  grep -o "^$pkgname-[0-9r].*pkg.tar.zst" | awk {print}))
+assets=($(gh release view -R $repo_full --json assets | jq -cr '.assets[].name' | grep -o "^$pkgname-[0-9r].*pkg.tar.zst" | awk {print}))
 if [ ${#assets[@]} -gt 0 ]; then
-  for asset in "${assets[@]}";do 
-    echo "Deleting asset: $asset"
-    gh release delete-asset $tag $asset -R $repo_full -y
-  done
+	for asset in "${assets[@]}"; do
+		echo "Deleting asset: $asset"
+		gh release delete-asset $tag $asset -R $repo_full -y
+	done
 else
-  echo "No asset found matching the pattern."
+	echo "No asset found matching the pattern."
 fi
