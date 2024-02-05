685270485e276d0d10c787abe4c9bf523aee1996
auryouth <ausosa528@proton.me>
2024-02-05 15:32:11 +0800

    update

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -7,14 +7,8 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 chmod -R a+rw .
 
-cat <<EOM >>/etc/pacman.conf
-[archlinuxcn]
-Server = https://repo.archlinuxcn.org/x86_64
-EOM
-
 pacman-key --init
-pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
-pacman -Syu --noconfirm paru
+pacman -Syu --noconfirm
 if [ -n "$INPUT_PREINSTALLPKGS" ]; then
 	pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
 fi
