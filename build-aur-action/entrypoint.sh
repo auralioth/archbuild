d5ec2dcfd994f2e859b6297374a601cbd12a9560
auryouth <ausosa528@proton.me>
2024-03-06 20:23:30 +0800

    refactor: build logic

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# 变量
 pkgname=$1
-repo_full=$2
+vfile=$2
 
+# 初始化
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 chmod -R a+rw .
@@ -14,22 +16,42 @@ EOM
 
 pacman-key --init
 pacman -Syu --noconfirm
-if [ -n "$INPUT_PREINSTALLPKGS" ]; then
-	pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
-fi
+pacman -S nvchecker --noconfirm
 
+# 确定打包文件
 if [ ! -d "$pkgname" ]; then
 	git clone https://aur.archlinux.org/$pkgname.git
 	chmod -R a+rw .
 fi
 
 cd "$pkgname" || exit
-status="true"
-gh release download -p "$pkgname-[0-9r]*pkg.tar.zst" -R $repo_full
-sudo -u builder makepkg -od --noprepare -A
-sudo -u builder bash -c 'export MAKEFLAGS=j$(nproc) && makepkg -s --noconfirm' || status=$?
+
+# 用于后续 delete-asset
+old_asset=$(basename $(sudo -u builder makepkg --packagelist))
+
+# Check 更新状态
+status="false"
+output=$(source PKGBUILD && declare -p pkgver || :)
+pattern='declare -- pkgver="([^"]+)"'
+
+if [[ $output =~ $pattern ]]; then
+	pkgver="${BASH_REMATCH[1]}"
+fi
+
+newver=$(nvchecker -t 3 --logger json -c $vfile | jq -r '.version')
+
+if [ "$pkgver" != "$newver" ]; then
+	status="true"
+	if ! grep -nq "^pkgver()" PKGBUILD; then
+		sed -i "s/pkgver=$pkgver/pkgver=$newver/" PKGBUILD
+	fi
+	updpkgsums
+	sudo -u builder bash -c 'export MAKEFLAGS=j$(nproc) && makepkg -s --noconfirm'
+fi
+
 asset=$(basename $(sudo -u builder makepkg --packagelist))
-new_ver=$(grep -oP 'pkgver=\K[^ ]+' PKGBUILD)
+
+echo "old_asset=$old_asset" >>$GITHUB_OUTPUT
 echo "status=$status" >>$GITHUB_OUTPUT
 echo "asset=$asset" >>$GITHUB_OUTPUT
-echo "new_ver=$new_ver" >>$GITHUB_OUTPUT
+echo "newver=$newver" >>$GITHUB_OUTPUT
