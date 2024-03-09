fd0b535ee3b9df3c40452a362328cc3de9694b0c
auryouth <ausosa528@proton.me>
2024-03-10 03:47:17 +0800

    fix: fix repo-add-action/entrypoint.sh

--- repo-add-action/entrypoint.sh
+++ repo-add-action/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-repo_onwer=$INPUT_REPO_ONWER
+repo_owner=$INPUT_REPO_ONWER
 repo_full=$INPUT_REPO_FULL
 path=$INPUT_PATH
 
 gh release download -p "$repo_onwer.db.tar.gz" -R $repo_full
 
-repo-add -R $path/$repo_onwer.db.tar.gz ./*.pkg.tar.zst
+repo-add -R $path/$repo_owner.db.tar.gz ./*.pkg.tar.zst
 ls -l
 for i in *.db *.files; do
 	cp --remove-destination $(readlink $i) $i
