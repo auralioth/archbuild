4452d0e2a622ddaf77a31062079132aba99a9923
auryouth <ausosa528@proton.me>
2024-07-09 01:15:03 +0800

    update

--- repo-action/entrypoint.sh
+++ repo-action/entrypoint.sh
@@ -7,27 +7,23 @@ request=$INPUT_REQUEST
 remove_pkgs=$INPUT_REMOVE_PKGS
 
 if gh release download -p "$repo_owner.db.tar.gz" -R $repo_full -D $path; then
-	echo "download $repo_owner.db.tar.gz"
-else
-	exit 0
+  echo "db.tar.gz exists. Download $repo_owner.db.tar.gz"
 fi
 
 if gh release download -p "$repo_owner.files.tar.gz" -R $repo_full -D $path; then
-	echo "download $repo_owner.files.tar.gz"
-else
-	exit 0
+  echo "files.tar.gz exists. Download $repo_owner.files.tar.gz"
 fi
 
 if [ $request = "add" ]; then
-	repo-add -R $path/$repo_owner.db.tar.gz ./*.pkg.tar.zst
-	for i in *.db *.files; do
-		cp --remove-destination $(readlink $i) $i
-	done
+  repo-add -R $path/$repo_owner.db.tar.gz ./*.pkg.tar.zst
+  for i in *.db *.files; do
+    cp --remove-destination $(readlink $i) $i
+  done
 fi
 
 if [ $request = "remove" ]; then
-	repo-remove $path/$repo_owner.db.tar.gz $remove_pkgs
-	for i in *.db *.files; do
-		cp --remove-destination $(readlink $i) $i
-	done
+  repo-remove $path/$repo_owner.db.tar.gz $remove_pkgs
+  for i in *.db *.files; do
+    cp --remove-destination $(readlink $i) $i
+  done
 fi
